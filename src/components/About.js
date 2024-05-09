c3f3254f8b5bc35184a1ef07aef508cbf2ce8230
AlexGathecha <alex.gathecha@student.moringaschool.com>
2024-05-09 22:41:15 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { image } from "../data/data";
+import React from 'react';
+import myImage from '../path/to/your/image.jpg'; // Import your image
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Your content here</p>
+      <img src={myImage} alt="I made this" />
+    </div>
+  );
+};
 
 export default About;
